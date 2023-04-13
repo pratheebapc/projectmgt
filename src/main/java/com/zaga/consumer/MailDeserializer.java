4b9c1bd239e774efadf841edad7f480693a81d48
Clyde Tedrick <clydetedrick@Clydes-MacBook-Pro.local>
2023-04-12 23:11:12 -0400

    build issues

--- src/main/java/com/zaga/consumer/MailDeserializer.java
+++ src/main/java/com/zaga/consumer/MailDeserializer.java
@@ -1,16 +1,12 @@
 package com.zaga.consumer;
 
 import com.zaga.event.EventDto;
-import com.zaga.model.entity.ProjectDetails;
-
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 
-public class MailDeserializer  extends ObjectMapperDeserializer<EventDto>{
+public class MailDeserializer  extends ObjectMapperDeserializer<EventDto> {
 
     public MailDeserializer() {
-        
         super(EventDto.class);
-        
     }
     
 }
